b05adb8b8046336e9460dc4138587247f4cb4dda
Vinnicius Oliveira Rodrigues <vinniciusrodrigues1999@gmail.com>
2024-05-14 02:02:49 -0300

    subindo erro 404 de rota não encontrada

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ server.use(
 
 server.use(router);
 
+server.use((req, res, next) => {
+  res.status(404).json({ error: 'Rota não encontrada' });
+});
+
 server.listen(3000, () => {
   console.log('JSON Server is running');
 });
